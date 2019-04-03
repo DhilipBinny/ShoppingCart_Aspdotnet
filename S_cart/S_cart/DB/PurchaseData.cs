e43095e842d2ffec550170b8ec5ac02473baa18f
Shuang <qingshuang.chan@gmail.com>
2019-04-03 16:32:30 +0800

    purchase db

--- S_cart/S_cart/DB/PurchaseData.cs
+++ S_cart/S_cart/DB/PurchaseData.cs
@@ -15,7 +15,7 @@ namespace S_cart.DB
         //Handle all queries relating to purchase
         public static List<Purchase> purchasesdb()
         {
-            List<Purchase> product1 = new List<Purchase>();
+            List<Purchase> purchase1 = new List<Purchase>();
             SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["conn"].ConnectionString);
             conn.Open();
             SqlCommand cmd = new SqlCommand("select p.product_id, count(p.product_id) as Quantity,  p.product_name,p.image_url, p.product_description from cart_info c join product_info p on c.product_id = p.product_id where c.user_id = 1 and c.status_code = 0 group by p.product_id,p.product_name,p.image_url,p.unit_price,p.product_description", conn);
@@ -23,19 +23,20 @@ namespace S_cart.DB
 
             while (rdr.Read())
             {
-                product prod = new product();
+                Purchase purc = new Purchase();
                 {
-                    prod.product_name = (string)rdr1["product_name"];
-                    prod.unit_price = (int)rdr1["unit_price"];
-                    prod.product_description = (string)rdr1["product_description"];
-                    prod.image = (string)rdr1["image_url"];
+                    //purc.product_name = (string)rdr["product_name"];
+                    //purc.unit_price = (int)rdr["unit_price"];
+                    //purc.product_description = (string)rdr["product_description"];
+                    //purc.image = (string)rdr["image_url"];
 
 
                 }
-                product1.Add(prod);
+                purchase1.Add(purc);
             }
-            con1.Close();
-            return product1;
+            conn.Close();
+            return purchase1;
 
-        }  
+        }
+    }
 }
